4936a17f0731e8028cb17c944a7843e2e7e6fe9c
iamkaniska <kaniskamaity@gmail.com>
2024-08-26 19:51:24 +0530

    BubbleSort

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,25 @@
+/* BubbleSorting */
+import java.util.*;
+public class bubbleSort{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
