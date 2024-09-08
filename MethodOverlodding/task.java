bea454e3a57cb2fb72703577236394823ce6d837
iamkaniska <kaniskamaity@gmail.com>
2024-09-09 01:29:22 +0530

    update

---
+++ MethodOverlodding/task.java
@@ -0,0 +1,16 @@
+public class task {
+    static int logic(int x, int y) {
+        int z;
+        z = x + y;
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 1;
+        int z;
+        z = logic(x, y);
+        System.out.println(z);
+    }
+
+}
