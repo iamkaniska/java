274ad65a0213cd5954bf6243ce69167a24dd37ed
iamkaniska <kaniskamaity@gmail.com>
2024-10-26 23:28:27 +0530

    update

---
+++ oops_java/constructor/constructor.java
@@ -0,0 +1,13 @@
+class m{
+        int roll_no=30;String name="Kaniska";
+        void data(){
+            System.out.println(roll_no+" "+name);
+        }
+    }
+class task{
+        public static void main(String[] args) {
+            m result=new m();
+            result.data();
+        }
+    }
+
